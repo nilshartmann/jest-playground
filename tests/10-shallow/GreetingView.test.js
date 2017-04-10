37b58bdddf6e925aec27553c7cea7f3a343a97af
Nils Hartmann <nils@nilshartmann.net>
2017-04-10 20:48:32 +0200

    Shallow example

--- tests/10-shallow/GreetingView.test.js
+++ tests/10-shallow/GreetingView.test.js
@@ -2,6 +2,21 @@ import React from 'react';
 import GreetingView from '../../src/GreetingView';
 import {shallow} from 'enzyme';
 
+const D = title => <div>{title}</div>;
+const C = title => <div>I'm C: <D title={title} /></div>;
+const B = title => <div>I'm B: <C title={title} /></div>;
+const A = title => <div>I'm A: <B title={title} /></div>;
+
+
+test('it should render A by default', () => {
+    const component = shallow(<A title="AAA" />);
+    console.log(component.debug());
+    expect(component.find('B')).toHaveLength(1);
+    expect(component.find('C')).toHaveLength(0);
+    expect(component.find('D')).toHaveLength(0);
+});
+
+
 
 test('it should render GreetingView by default', () => {
     const component = shallow(<GreetingView/>);
